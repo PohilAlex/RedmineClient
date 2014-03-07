2937bdf3cb8edfec9d61fcb94828dc08eb25e755
Alexander Pohil <pohil@mlsdev.com>
2014-03-07 11:08:48 +0200

    Changed app name

--- platforms/android/assets/www/js/controller/loginCtrl.js
+++ platforms/android/assets/www/js/controller/loginCtrl.js
@@ -5,12 +5,15 @@ angular.module('rcApp').controller('loginCtrl', ['$scope', '$http', '$location',
     var user = localStorage.getItem('user');//JSON.parse(localStorage.getItem('user'));
     var api_key;
     if (user != "undefined") {
-      api_key = JSON.parse(user).api_key;
+      if (JSON.parse(user) != undefined && api_key in JSON.parse(user)) {
+        api_key = JSON.parse(user).api_key;
+      }
     }
     if (api_key != undefined) {
       $location.path('/projects');
     }
   	$scope.login = function() {
+      console.log('Login ' + $scope.user.name + ":" + $scope.user.pass);
 	    $http({method: 'GET', url: loginUrl, headers: {'Authorization': 'Basic ' + window.btoa($scope.user.name + ":" + $scope.user.pass)}})
       //$http({method: 'GET', url: loginUrl, headers: {'Authorization': 'Basic ' + window.btoa(username + ":" + password)}})
 	    	.success(function(data, status, headers, config) {
